0f8c962675f039900760224d36514d3191923377
Stefan Stadlberger <github@email.stadlberger.com>
2022-09-30 17:13:31 +0200

    next steps

--- OLED_Display/start.sh
+++ OLED_Display/start.sh
@@ -10,7 +10,7 @@ if ls /dev/i2c-1; then
     bashio::log.info "I2C access OK";
     bashio::log.info "Displaying info on OLED"
     cd /SSD1306OLED/
-    python3 stats.py
+    python3 stats.py --mode hassio
 else
     bashio::log.info "No I2C access.";
 fi
