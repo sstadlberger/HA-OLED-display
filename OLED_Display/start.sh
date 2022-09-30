4635373180dd86afee6bc8dcf0ed08317753770e
Stefan Stadlberger <github@email.stadlberger.com>
2022-09-30 12:42:12 +0200

    Housekeeping

--- OLED_Display/start.sh
+++ OLED_Display/start.sh
@@ -5,22 +5,22 @@ set -e
 CONFIG_PATH=/data/options.json
 
 DISABLE_AUTO_START="$(bashio::config 'Stop_Auto_Run')"
-bashio::log.info "Starting UCTronics OLED App..."
+bashio::log.info "Starting OLED display..."
 bashio::log.info "Disable Auto Start = ${DISABLE_AUTO_START}"
 
 
 if [ "$DISABLE_AUTO_START" = false ]; then
 
     if ls /dev/i2c-1; then 
-        bashio::log.info "Got i2c access! WoHOo!";
-        bashio::log.info "Display Info to OLED"
-        cd /UCTronics_OLED/
+        bashio::log.info "I2C access OK";
+        bashio::log.info "Displaying info on OLED"
+        cd /SSD1306OLED/
         python3 display.py
     else
-        bashio::log.info "No i2c Access!! Please read the instructions.";   
+        bashio::log.info "No I2C access.";
     fi 
 
 else
-    bashio::log.info "No Auto Run"
+    bashio::log.info "No auto run"
     sleep 99999;
 fi
