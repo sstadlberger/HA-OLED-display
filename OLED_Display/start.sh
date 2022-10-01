44a3cac7a25d7eb106eacca2879639d4034d39c9
Stefan Stadlberger <github@email.stadlberger.com>
2022-10-01 13:25:24 +0200

    Hmmm...

--- OLED_Display/start.sh
+++ OLED_Display/start.sh
@@ -8,6 +8,8 @@ if ls /dev/i2c-1; then
     bashio::log.info "I2C access OK"
     bashio::log.info "Displaying info on OLED"
     cd /SSD1306OLED/
+    curl -sSL -H "Authorization: Bearer $SUPERVISOR_TOKEN" http://supervisor/host/info
+    curl -sSL -H "Authorization: Bearer ${SUPERVISOR_TOKEN}" http://supervisor/host/info
     python3 stats.py --mode hassio
 else
     bashio::log.info "No I2C access."
