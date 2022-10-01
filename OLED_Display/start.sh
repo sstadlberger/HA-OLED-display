409a245fd45a5ae35afab8a7c32298c346490ef2
Stefan Stadlberger <github@email.stadlberger.com>
2022-10-01 12:21:55 +0200

    variables...

--- OLED_Display/start.sh
+++ OLED_Display/start.sh
@@ -9,7 +9,7 @@ bashio::log.info "Starting OLED display..."
 if ls /dev/i2c-1; then 
     bashio::log.info "I2C access OK"
     bashio::log.info "Displaying info on OLED"
-    export SUPERVISOR_TOKEN = "$(bashio::config 'SUPERVISOR_TOKEN')"
+    export SUPERVISOR_TOKEN="$(bashio::config 'SUPERVISOR_TOKEN')"
     bashio::log.info "Token is = ${SUPERVISOR_TOKEN}"
     cd /SSD1306OLED/
     python3 stats.py --mode hassio
